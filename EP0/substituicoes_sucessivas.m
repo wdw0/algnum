0eb926e6c2eed87d00aeca5cdff397539a51bd3e
gabriel <101599265+wdw0@users.noreply.github.com>
2024-11-21 10:45:19 -0300

    Add files via upload

---
+++ EP0/substituicoes_sucessivas.m
@@ -0,0 +1,10 @@
+function [x] = substituicoes_sucessivas(n, L, c)
+   x(1) = c(1) / L(1,1);
+   for i=2:n
+    Soma = 0;
+    for j=1:i-1
+       Soma = Soma + L(i,j) * x(j);
+    endfor
+    x(i) = (c(i) - Soma) / L(i,i);
+  endfor
+endfunction
